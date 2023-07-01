50b6c6ae0cbc01aa35b6aaface342c1b9775424c
5had0w11 <soubesda92@gmail.com>
2023-07-01 10:09:38 +0200

    Finished quiz game with UI and functionality, the final Q and A data are missing

--- NihonNoLife/Assets/NihonNoLife/Resources/Scripts/Quiz/AnswerScript.cs
+++ NihonNoLife/Assets/NihonNoLife/Resources/Scripts/Quiz/AnswerScript.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AnswerScript : MonoBehaviour
 {
     public bool isCorrect = false;
     public QuizManager quizMger;
+
+    //called whenever an answer button is pressed. Depending on the answer, it will call the correct or wrong function from the quiz manager. 
     public void Answer()
     {
         if (isCorrect)
         {
-            Debug.Log("Correct Answer");
             quizMger.Correct();
-
         }
         else
         {
-            Debug.Log("Incorrect Answer");
+            quizMger.Wrong();
         }
+
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
--- NihonNoLife/Assets/NihonNoLife/Resources/Scripts/Quiz/QuizManager.cs
+++ NihonNoLife/Assets/NihonNoLife/Resources/Scripts/Quiz/QuizManager.cs
@@ -5,21 +5,55 @@ using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
+    [Header("Questions")]
     public List<QuestionsAnswers> QnA;
     public GameObject[] options;
     public int currentQuestion;
 
+    [Header("Text")]
     public TextMeshProUGUI questionText;
+    public TextMeshProUGUI backgroundText;
+    public TextMeshProUGUI correctNumber;
+    public TextMeshProUGUI wrongNumber;
 
+    [SerializeField] private string correctText;
+    [SerializeField] private string wrongText;
+
+    [Header("Panels")]
+    public GameObject quizPanel;
+    public GameObject resultsPanel;
+
+    [SerializeField] private float waitTime;
+
+    private int correctAnswers = 0;
+    private int wrongAnswers = 0;
+
+    //making sure the correct panels are active at the beginning of the quiz and generate one question from the pool at random
     private void Start()
     {
-        
+        backgroundText.text = null;
+        resultsPanel.SetActive(false);
+        quizPanel.SetActive(true);
+        GenerateQuestion();
     }
 
+    //called when the selected answer is correct. It adds to the correct score and displays the correct message
     public void Correct()
     {
-        QnA.RemoveAt(currentQuestion);
+        correctAnswers++;
+
+        StartCoroutine(CorrectMessage());
     }
+
+    //called when the selected answer is wrong. It adds to the wrong score and displays the wrong message
+    public void Wrong()
+    {
+        wrongAnswers++;
+
+        StartCoroutine(WrongMessage());
+    }
+
+    //it sets the multiple choice answers in the buttons depending on the question that has been generated. 
     private void SetAnswers()
     {
         for (int i = 0; i < options.Length; i++)
@@ -35,12 +69,58 @@ public class QuizManager : MonoBehaviour
         }
     }
 
+    //generates one question at random from the pool. It is called at the beginning of the quiz and after each answer. When there are no more questions available, it moves to the final score
     private void GenerateQuestion()
     {
-        currentQuestion = Random.Range(0, QnA.Count);
+        if (QnA.Count > 0)
+        {
+            currentQuestion = Random.Range(0, QnA.Count);
 
-        questionText.text = QnA[currentQuestion].question;
+            questionText.text = QnA[currentQuestion].question;
 
-        SetAnswers();
+            SetAnswers();
+        }
+        else
+        {
+            quizPanel.SetActive(false);
+
+            correctNumber.text = correctAnswers.ToString();
+            wrongNumber.text = wrongAnswers.ToString();
+
+            resultsPanel.SetActive(true);
+        }
+        
+    }
+
+    //displays the "correct!" message for a set duration before moving to the next question
+    IEnumerator CorrectMessage()
+    {
+        backgroundText.text = correctText;
+        quizPanel.SetActive(false);
+
+        yield return new WaitForSeconds(waitTime);
+
+        QnA.RemoveAt(currentQuestion);
+        backgroundText.text = null;
+        quizPanel.SetActive(true);
+        GenerateQuestion();
+
+        StopCoroutine(CorrectMessage());
+    }
+
+    //displays the "wrong" message for a set duration before moving to the next question
+    IEnumerator WrongMessage()
+    {
+        backgroundText.text = wrongText;
+        quizPanel.SetActive(false);
+
+        yield return new WaitForSeconds(waitTime);
+
+        QnA.RemoveAt(currentQuestion);
+        backgroundText.text = null;
+        quizPanel.SetActive(true);
+        GenerateQuestion();
+
+        StopCoroutine(WrongMessage());
     }
 }
